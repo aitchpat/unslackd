891683fff4792236dd5324f0c04c417a7956fad7
aitchpat <harry@thinkhere.com>
2018-03-30 00:14:46 -0400

    further organization

--- src/routes/interactive.js
+++ src/routes/interactive.js
@@ -1,18 +1,9 @@
-import createMessageAdapter from '@slack/interactive-messages';
+// Controller
+import InteractiveController from '../controllers/interactive';
 
-const slackMessages = createMessageAdapter(process.env.SLACK_VERIFICATION_TOKEN);
-
-slackMessages.action('interactive_beer_search', (payload) => {
-  // `payload` is JSON that describes an interaction with a message.
-  console.log(`The user ${payload.user.name} in team ${payload.team.domain} pressed the load more button`);
-
-  // The `actions` array contains details about the specific action (button press, menu selection, etc.)
-  const action = payload.actions[0];
-  console.log(`The button had name ${action.name} and value ${action.value}`);
-});
 
 const createInteractiveRoutes = (router) => {
-  router.post('/interactive', slackMessages.expressMiddleware());
+  router.post('/interactive', InteractiveController.logMessages);
 };
 
 export default createInteractiveRoutes;
